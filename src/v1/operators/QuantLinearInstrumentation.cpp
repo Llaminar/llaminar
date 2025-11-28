054e3cbcf7bafa490f649231e0f52ea70554423e
David Sanftenberg <david.sanftenberg@gmail.com>
2025-11-28 12:22:50 +0000

    cleanup old/unused files, remove old v1 source, remove COSMA

--- src/v1/operators/QuantLinearInstrumentation.cpp
+++
@@ -1,9 +0,0 @@
-#include "QuantLinearInstrumentation.h"
-namespace llaminar
-{
-    QuantLinearInstrumentation &quantLinearInstr()
-    {
-        static QuantLinearInstrumentation inst;
-        return inst;
-    }
-}
